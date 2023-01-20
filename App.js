c9f1a2bcd885c61883417936c54f484bdde93f03
MehKad <mehkadiri@gmail.com>
2023-01-20 16:51:24 +0000

    Starting the home screen

--- App.js
+++ App.js
@@ -1,14 +1,18 @@
-import { StatusBar } from "expo-status-bar";
-import { Text, SafeAreaView } from "react-native";
 import { TailwindProvider } from "tailwindcss-react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Home from "./view/screens/Home";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <TailwindProvider>
-      <SafeAreaView>
-        <Text>Open up App.js to start working on your app!</Text>
-        <StatusBar style="auto" />
-      </SafeAreaView>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={Home} />
+        </Stack.Navigator>
+      </NavigationContainer>
     </TailwindProvider>
   );
 }
---
+++ view/screens/Home.js
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const Home = () => {
+  return (
+    <View>
+      <Text className="flex-1">Home</Text>
+    </View>
+  );
+};
+
+export default Home;
